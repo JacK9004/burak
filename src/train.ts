9df807903e44b82ff03566325d82f7292d9db250
JacK9004 <kjushkin@gmail.com>
2024-02-28 08:27:57 +0900

    feat: train TASK-H

--- src/train.ts
+++ src/train.ts
@@ -1,8 +1,51 @@
 // TASK-H
 
-function teskariOqish(a: string): string {
-    return a.split('').reverse().join('');
-}
+function musbatSonlar(arr: number[]): string 
+    {
+        const musbatQiymatlar = arr.filter(num => num > 0);
+        const natija = musbatQiymatlar.join('');
+        return natija;
+    }
 
-console.log(teskariOqish("jushkin"));
-console.log(teskariOqish("hello"));
+    console.log(musbatSonlar([1, -4, 2]));
+    console.log(musbatSonlar([21, 14, -6]));
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // TASK-H
+
+// function teskariOqish(a: string): string {
+//     return a.split('').reverse().join('');
+// }
+
+// console.log(teskariOqish("jushkin"));
+// console.log(teskariOqish("hello"));
